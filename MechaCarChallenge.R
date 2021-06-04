e7c7ad597725f4ebf700353ecec1934f14cdee7b
Dean Arnold <dean-arnold@live.com>
2021-06-04 15:04:08 -0400

    d3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,16 @@ sus_coil <- read.csv(file='Suspension_Coil.csv', check.names=F,stringsAsFactors 
 
 summarize_demo <- sus_coil%>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Standard_Deviation=sd(PSI), .groups = 'keep')
 
+?t.test()
+
+t.test((sus_coil$PSI), mu=1500)
+
+lot1 <- subset(sus_coil, Manufacturing_Lot=='Lot1') 
+t.test(x=lot1$PSI, mu=1500) 
+
+lot2 <- subset(sus_coil, Manufacturing_Lot=='Lot2') 
+t.test(x=lot2$PSI, mu=1500) 
+
+lot3 <- subset(sus_coil, Manufacturing_Lot=='Lot3') 
+t.test(x=lot3$PSI, mu=1500) 
+
